33616ad5f7c16d41acf784ca84375cd47078e296
OblivionCreator <theoblivioncreator@gmail.com>
2024-07-29 00:34:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker system prune
-docker build -t mettaton-3 .
-docker save mettaton-3 -o ~/docker-store/mettaton-3.tar
+docker build -t mettaton-3-new .
+docker save mettaton-3 -o ~/docker-store/mettaton-3-new.tar
